06df7877d07ba34a3ff90b762d51d07657d4ac50
mohammad dehdar <dehdardevelop@gmail.com>
2024-08-12 18:52:45 +0330

    recieve data by name

--- public/app.js
+++ public/app.js
@@ -1,2 +1,25 @@
-const   BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=7ecbcfc5912359e54c3c1271a7f8ff42&units=metric"
-const API_key = "7ecbcfc5912359e54c3c1271a7f8ff42"
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
+const API_KEY = "7ecbcfc5912359e54c3c1271a7f8ff42"
+
+const searchInput = document.querySelector("input");
+const searchButton = document.querySelector("button");
+
+
+const getCurrentWeatherByName = async city => {
+    const url =`${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
+    console.log(url);
+    const response = await fetch(url)
+    const json = await response.json();
+    return json;
+}
+
+const searchHandler = async () => {
+    const cityName = searchInput.value;
+    if (!cityName) {
+        alert("Please select a city");
+    }
+    const currentData = await getCurrentWeatherByName(cityName)
+    console.log(currentData);
+    
+}
+searchButton.addEventListener("click", searchHandler)
